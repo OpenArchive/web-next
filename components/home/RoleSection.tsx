adeab97605298b75980dfa7c2b0ad86f8c5258d7
Joseph Lacey <joseph@offkeydigital.com>
2024-10-22 11:40:31 -0700

    Fixing linting errors from previous commit

--- components/home/RoleSection.tsx
+++ components/home/RoleSection.tsx
@@ -1,10 +1,9 @@
 import { FC, PropsWithChildren } from "react";
 import { Box, Grid } from "@mui/material";
-import { typography, colors, breakpoints } from "styles/theme";
+import { colors, breakpoints } from "styles/theme";
 import { PageSection } from "components/common/PageSection";
 
 export const RoleSection: FC<PropsWithChildren> = ({ children }) => {
-  const { bodyLarge } = typography;
   const { white } = colors;
   const { ps, ts } = breakpoints;
   const [title, item1, item2, item3] = children as any[];
@@ -14,21 +13,6 @@ export const RoleSection: FC<PropsWithChildren> = ({ children }) => {
       <Grid container direction="column">
         <Grid item sx={{ textAlign: "center" }}>
           <Box sx={{ h2: { mb: 2 } }}>{title}</Box>
-          <Box
-            sx={{
-              margin: "0 auto",
-              "> p": bodyLarge,
-              mb: 6,
-              [ps]: {
-                mb: 4,
-              },
-              [ts]: {
-                mb: 6,
-              },
-              maxWidth: 700,
-            }}
-          >
-          </Box>
         </Grid>
         <Grid
           item
